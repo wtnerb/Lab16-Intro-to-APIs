8d2eab6745bfcff9e32ee465636c2180e7b7dc08
wtnerb <wtnerb@gmail.com>
2018-04-11 09:02:08 -0700

    I should have committed earlier

---
+++ TodoApi/TodoApi/Models/TodoContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        //MVC magic
+        //This is essential to the api interfacing with the database
+        public TodoContext (DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
